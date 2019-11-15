c31018f5e523171dee5417abd5ad362518b3c350
Anudeep Reddy <anudeepc85@gmail.com>
2019-11-15 12:25:16 +0000

    exams time

--- exams/2_indexed.cpp
+++ exams/2_indexed.cpp
@@ -1,1 +1,39 @@
-//TODO
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int TOTAL_SPACE=1024,FREE_SPACE=1024;
+struct memory{
+    bool isfree;
+    struct memory *next[1024];
+};
+int main(){
+    int n,start,size;
+    cin>>n;
+    memory mem[TOTAL_SPACE];
+    for(int i=0;i<TOTAL_SPACE;i++){
+        mem[i].isfree=true;
+    }
+    for(int i=0;i<n;i++){
+        cin>>start>>size;
+        if(mem[start].isfree!=true){
+            cout<<"already occupied";
+            continue;
+        }
+        if(size>FREE_SPACE){
+            cout<<"No enough space";
+            continue;
+        }
+        int randblock;
+        FREE_SPACE--;
+        cout<<"Allocated:"<<start;
+        mem[start].isfree=false;
+        for(int j=1;j<size;){
+            randblock=rand()%TOTAL_SPACE;
+            if(mem[randblock].isfree){
+                cout<<"->"<<randblock;
+                mem[start].next[j-1]=&mem[randblock];
+                mem[randblock].isfree=false;
+            }
+        }
+    }
+}
--- exams/2_linked.cpp
+++ exams/2_linked.cpp
@@ -1,7 +1,44 @@
 #include<bits/stdc++.h>
-using namesapce std;
-//structure goes here
-//TODO
+using namespace std;
+int TOTAL_SPACE=1024,FREE_SPACE=1024;
+struct memory{
+    bool isfree;
+    struct memory *next;
+};
 int main(){
-
+    int n,start,size;
+    cout<<"Enter the number of processes:";
+    cin>>n;
+    memory mem[TOTAL_SPACE];
+    for(int i=0;i<TOTAL_SPACE;i++){
+        mem[i].isfree=true;
+    }
+    for(int i=0;i<n;i++){
+        cout<<"Enter the starting block and size of process "<<i<<":";
+        cin>>start>>size;
+        if(!mem[start].isfree){
+            cout<<"The starting block is already occupied";
+            continue;
+        }
+        if(size>FREE_SPACE){
+            cout<<"No enough space to allocate memory";
+            continue;
+        }
+        int randblock;
+        mem[start].isfree=false;
+        memory *tmp=&mem[start];
+        cout<<"Allocated:"<<start;
+        for(int j=1;j<size;){
+            randblock=rand()%TOTAL_SPACE;
+            if(mem[randblock].isfree){
+                j++;
+                mem[randblock].isfree=false;
+                tmp->next=&mem[randblock];
+                tmp=tmp->next;
+                FREE_SPACE--;
+            cout<<"->"<<randblock;
+            }
+        }
+        cout<<"\n";
+    }
 }
